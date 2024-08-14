826ae182ea3fec7514660cbf03e5df481057f32b
caiolaytynher <csrlaytynher@proton.me>
2024-08-14 12:13:21 -0300

    after second lesson

--- compuctory/entry_compuctory.c
+++ compuctory/entry_compuctory.c
@@ -7,16 +7,31 @@ int entry(int argc, char** argv) {
     window.y = 90;
     window.clear_color = hex_to_rgba(0x1f262dff);
 
+    Vector2 sprite_size = v2(8.0f, 8.0f);
     Gfx_Image* player = load_image_from_disk(
         STR("assets/sprites/player.png"), get_heap_allocator()
     );
-    Vector2 player_position = v2(0.0f, 0.0f);
-    f32 player_speed = 1.0f;
-    Vector2 sprite_size = v2(0.5f, 0.5f);
+    // Centered on x axis
+    Vector2 player_position = v2(-sprite_size.x / 2.0f, 0.0f);
+    f32 player_speed = 100.0f;
+
+    // Make it 1:1 to pixel size
+    Matrix4 projection = m4_make_orthographic_projection(
+        -(f32)window.width / 2.0f, (f32)window.width / 2.0f,
+        -(f32)window.height / 2.0f, (f32)window.height / 2.0f, -1, -10
+    );
+
+    // Zoom to the appropriate viewport
+    f32 viewport = 240.0f;
+    Matrix4 view = m4_make_scale(
+        v3(viewport / window.width, viewport / window.width, 1.0f)
+    );
 
     f64 dt = 0.0;
     f64 last_time = os_get_current_time_in_seconds();
     while (!window.should_close) {
+        reset_temporary_storage();
+
         // Display FPS on the console
         f64 now = os_get_current_time_in_seconds();
         if ((int)now != (int)last_time) {
@@ -26,7 +41,8 @@ int entry(int argc, char** argv) {
         dt = now - last_time;
         last_time = now;
 
-        reset_temporary_storage();
+        draw_frame.projection = projection;
+        draw_frame.view = view;
 
         if (is_key_just_pressed(KEY_ESCAPE)) {
             window.should_close = true;
